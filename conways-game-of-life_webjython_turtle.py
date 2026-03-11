0ec282d74fe393ddf4bb3495cae157891f5493bb
Lennart Schmehr <lennart.schmehr@proton.me>
2026-03-11 19:45:53 +0100

    Add GitHub link for web code

--- conways-game-of-life_webjython_turtle.py
+++ conways-game-of-life_webjython_turtle.py
@@ -1,6 +1,6 @@
 # Conways Game of Life - WebJython Turtle Version
 # © 2026 XeraoXX & Github Contributors | Licensed under GNU GPL v3
-
+# https://github.com/Nolocon-Projects/Conways-Turtle
 # This project requires WebTigerJython. Open it on https://webtigerjython.ethz.ch/
 from gturtle import *
 import time
@@ -112,4 +112,5 @@ class logicManager():
             time.sleep(waitingDuration)
         
 if __name__ == "__main__":
+
     logicManager().run()
